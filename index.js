05704075d6b77d053ea9d455ea13ddb04622d42a
Shivam Kumar <kumarshivam16062004@gmail.com>
2022-12-07 14:27:33 +0530

    version 1.0

---
+++ index.js
@@ -0,0 +1,66 @@
+// console.log('hello world')
+//creating a variable
+// let num = 10
+// let message = 'Hello'
+
+
+// let num = 100, message = 'greetings'
+
+//automatically adds a semicolon
+// let num = 10;
+// console.log(num);
+// let num = 100;
+// console.log(num);
+
+
+//interaction
+
+// alert('hello world')
+
+// let askFromUser = prompt('Enter a number')
+// console.log(askFromUser)
+
+
+// let isProgrammer = confirm('Are you a programmer')
+
+// console.log(isProgrammer)
+
+
+//single line comment
+
+/*let
+a =
+10 multiline comment */
+
+//common datatypes 
+//String => text
+//Numbers => int as well as float
+//Arrays => ordered pair of collection [10,20,30]
+//Objects => key value pair {name: 'John'}
+//boolean => true and false
+
+// {} => a block
+//Conditional statements
+// if (true) {
+//     alert('hello')
+// }
+// alert('world')
+
+
+// let year = 2012
+// if (year == 2022) {
+//     alert('You are so smart')
+// } else {
+//     alert('you are so dumb')
+// }
+
+let age = prompt('How old are you')
+
+if (age < 18) {
+    alert('access not allowed ')
+} else if (age < 20) {
+    alert('you have limited access')
+} else {
+    alert('here is you pass to the theater')
+
+}
